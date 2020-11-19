803476f15b49fdddc454c350d235bb88a534badd
rucko24 <rucko24@gmail.com>
2020-11-19 03:35:52 +0100

    change names

--- src/test/java/com/flapdoodle/reactivemongo/ReactiveMongoWithFlapdoodleTest.java
+++ src/test/java/com/flapdoodle/reactivemongo/ReactiveMongoWithFlapdoodleTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DemoApplicationTests {
+class ReactiveMongoWithFlapdoodleTest {
 
 	@Test
 	void contextLoads() {
